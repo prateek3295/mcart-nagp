2fb0822676fd2fa689e203b6f7616232dd728c2d
Prateek <prateek.sharma03@nagarro.com>
2024-03-10 23:05:03 +0530

    fix url

--- mcart.ui/src/app/product-api.service.ts
+++ mcart.ui/src/app/product-api.service.ts
@@ -8,7 +8,7 @@ import { fetchAuthSession } from 'aws-amplify/auth';
 })
 export class ProductApiService {
 
-  private apiUrl = "hcatalog-lb-945085276.us-east-1.elb.amazonaws.com/api/v1/catalog/ ;
+  private apiUrl = "hcatalog-lb-945085276.us-east-1.elb.amazonaws.com/api/v1/catalog/" ;
 
   constructor(private http: HttpClient) {}
 
